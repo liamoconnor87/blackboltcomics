028904285ad91039c36eae6efdbf2916a5021cb6
liamoconnor87 <liamoconnor87@gmail.com>
2021-04-01 10:38:13 +0000

    Static files connected to pages.

--- blackbolt_comics/settings.py
+++ blackbolt_comics/settings.py
@@ -149,4 +149,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# connects static, etc files
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
