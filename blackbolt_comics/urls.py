7cdc5b3bdcbf65082ac54827ea1b6d97bfbdfe20
liamoconnor87 <liamoconnor87@gmail.com>
2021-04-01 11:21:03 +0000

    HTML&CSS:inital navbar creation.

--- blackbolt_comics/urls.py
+++ blackbolt_comics/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 from django.conf.urls.static import static
 
 
@@ -22,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_Root)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
