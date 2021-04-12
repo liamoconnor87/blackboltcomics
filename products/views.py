85515f81a0664a59d2c1f6235aba7cc6c9b2a021
liamoconnor87 <liamoconnor87@gmail.com>
2021-04-12 14:03:44 +0000

    Products by Category functioning.

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 def all_products(request):
@@ -9,8 +9,16 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        # Products Menu.
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        # Search Bar.
         if 'search-query' in request.GET:
             query = request.GET['search-query']
             if not query:
@@ -25,6 +33,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
