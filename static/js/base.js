dbf55685a499130b947102e35eb7ff991906efe1
liamoconnor87 <liamoconnor87@gmail.com>
2021-04-13 13:02:29 +0000

    JS: Sort Selector functions on products.

--- static/js/base.js
+++ static/js/base.js
@@ -3,118 +3,140 @@ $(document).ready(function() {
     // Navigation Animations. 
     $(".srch-btn").click(function(){
 
-                $(this).toggleClass("srch-btn-click");
+        $(this).toggleClass("srch-btn-click");
 
-                $(".search-tool").toggle();
+        $(".search-tool").toggle();
 
-                $(".account-menu").hide();
-                $(".all-prod-menu").hide();
-                $(".comic-menu").hide();
-                $(".access-menu").hide();
-                $(".spec-off-menu").hide();
+        $(".account-menu").hide();
+        $(".all-prod-menu").hide();
+        $(".comic-menu").hide();
+        $(".access-menu").hide();
+        $(".spec-off-menu").hide();
 
-                if ($(".acc-btn").hasClass("acc-btn-click")){
-                    $(".acc-btn").removeClass("acc-btn-click");
-                }
+        if ($(".acc-btn").hasClass("acc-btn-click")) {
+            $(".acc-btn").removeClass("acc-btn-click");
+        }
 
-            })
+    })
 
-            $(".acc-btn").click(function(){
+    $(".acc-btn").click(function () {
 
-                $(this).toggleClass("acc-btn-click");
+        $(this).toggleClass("acc-btn-click");
 
-                $(".account-menu").toggle();
+        $(".account-menu").toggle();
 
-                $(".search-tool").hide();
-                $(".all-prod-menu").hide();
-                $(".comic-menu").hide();
-                $(".access-menu").hide();
-                $(".spec-off-menu").hide();
+        $(".search-tool").hide();
+        $(".all-prod-menu").hide();
+        $(".comic-menu").hide();
+        $(".access-menu").hide();
+        $(".spec-off-menu").hide();
 
-                if ($(".srch-btn").hasClass("srch-btn-click")){
-                    $(".srch-btn").removeClass("srch-btn-click");
-                }
+        if ($(".srch-btn").hasClass("srch-btn-click")) {
+            $(".srch-btn").removeClass("srch-btn-click");
+        }
 
-            })
+    })
 
-            $(".all-prod-click").click(function(){
-                
-                if ($(".acc-btn").hasClass("acc-btn-click")){
-                    $(".acc-btn").removeClass("acc-btn-click");
-                }
+    $(".all-prod-click").click(function () {
 
-                if ($(".srch-btn").hasClass("srch-btn-click")){
-                    $(".srch-btn").removeClass("srch-btn-click");
-                }
+        if ($(".acc-btn").hasClass("acc-btn-click")) {
+            $(".acc-btn").removeClass("acc-btn-click");
+        }
 
-                $(".all-prod-menu").toggle();
+        if ($(".srch-btn").hasClass("srch-btn-click")) {
+            $(".srch-btn").removeClass("srch-btn-click");
+        }
 
-                $(".comic-menu").hide();
-                $(".access-menu").hide();
-                $(".spec-off-menu").hide();
-                $(".search-tool").hide();
-                $(".account-menu").hide();
+        $(".all-prod-menu").toggle();
 
-            })
+        $(".comic-menu").hide();
+        $(".access-menu").hide();
+        $(".spec-off-menu").hide();
+        $(".search-tool").hide();
+        $(".account-menu").hide();
 
-            $(".comic-book-click").click(function(){
+    })
 
-                if ($(".acc-btn").hasClass("acc-btn-click")){
-                    $(".acc-btn").removeClass("acc-btn-click");
-                }
+    $(".comic-book-click").click(function () {
 
-                if ($(".srch-btn").hasClass("srch-btn-click")){
-                    $(".srch-btn").removeClass("srch-btn-click");
-                }
+        if ($(".acc-btn").hasClass("acc-btn-click")) {
+            $(".acc-btn").removeClass("acc-btn-click");
+        }
 
-                $(".comic-menu").toggle();
+        if ($(".srch-btn").hasClass("srch-btn-click")) {
+            $(".srch-btn").removeClass("srch-btn-click");
+        }
 
-                $(".all-prod-menu").hide();
-                $(".access-menu").hide();
-                $(".spec-off-menu").hide();
-                $(".search-tool").hide();
-                $(".account-menu").hide();
+        $(".comic-menu").toggle();
 
-            })
+        $(".all-prod-menu").hide();
+        $(".access-menu").hide();
+        $(".spec-off-menu").hide();
+        $(".search-tool").hide();
+        $(".account-menu").hide();
 
-            $(".access-click").click(function(){
+    })
 
-                if ($(".acc-btn").hasClass("acc-btn-click")){
-                    $(".acc-btn").removeClass("acc-btn-click");
-                }
+    $(".access-click").click(function () {
 
-                if ($(".srch-btn").hasClass("srch-btn-click")){
-                    $(".srch-btn").removeClass("srch-btn-click");
-                }
+        if ($(".acc-btn").hasClass("acc-btn-click")) {
+            $(".acc-btn").removeClass("acc-btn-click");
+        }
 
-                $(".access-menu").toggle();
+        if ($(".srch-btn").hasClass("srch-btn-click")) {
+            $(".srch-btn").removeClass("srch-btn-click");
+        }
 
-                $(".all-prod-menu").hide();
-                $(".comic-menu").hide();
-                $(".spec-off-menu").hide();
-                $(".search-tool").hide();
-                $(".account-menu").hide();
+        $(".access-menu").toggle();
 
-            })
+        $(".all-prod-menu").hide();
+        $(".comic-menu").hide();
+        $(".spec-off-menu").hide();
+        $(".search-tool").hide();
+        $(".account-menu").hide();
 
-            $(".offer-click").click(function(){
+    })
 
-                if ($(".acc-btn").hasClass("acc-btn-click")){
-                    $(".acc-btn").toggleClass("acc-btn-click");
-                }
+    $(".offer-click").click(function () {
 
-                if ($(".srch-btn").hasClass("srch-btn-click")){
-                    $(".srch-btn").toggleClass("srch-btn-click");
-                }
+        if ($(".acc-btn").hasClass("acc-btn-click")) {
+            $(".acc-btn").toggleClass("acc-btn-click");
+        }
 
-                $(".spec-off-menu").toggle();
+        if ($(".srch-btn").hasClass("srch-btn-click")) {
+            $(".srch-btn").toggleClass("srch-btn-click");
+        }
 
-                $(".all-prod-menu").hide();
-                $(".access-menu").hide();
-                $(".comic-menu").hide();
-                $(".search-tool").hide();
-                $(".account-menu").hide();
+        $(".spec-off-menu").toggle();
 
-            })
+        $(".all-prod-menu").hide();
+        $(".access-menu").hide();
+        $(".comic-menu").hide();
+        $(".search-tool").hide();
+        $(".account-menu").hide();
+
+    })
+
+    // Sort Selection Box
+    $('#sort-selector').change(function() {
+        var selector = $(this);
+        var currentUrl = new URL(window.location);
+
+        var selectedVal = selector.val();
+        if (selectedVal != "reset") {
+            var sort = selectedVal.split("_")[0];
+            var direction = selectedVal.split("_")[1];
+
+            currentUrl.searchParams.set("sort", sort);
+            currentUrl.searchParams.set("direction", direction);
+
+            window.location.replace(currentUrl);
+        } else {
+            currentUrl.searchParams.delete("sort");
+            currentUrl.searchParams.delete("direction");
+
+            window.location.replace(currentUrl);
+        }
+    })
 
 });
