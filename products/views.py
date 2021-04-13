d12d3243be5b749b2236f6f885a65dc67f9e3775
liamoconnor87 <liamoconnor87@gmail.com>
2021-04-13 12:37:28 +0000

    Initial Sorting and product counts into product page added.

--- products/views.py
+++ products/views.py
@@ -25,6 +25,9 @@ def all_products(request):
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
 
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
             if 'direction' in request.GET:
                 sortkey = f'-{sortkey}'
 
