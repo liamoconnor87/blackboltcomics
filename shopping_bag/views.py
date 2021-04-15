fe217390ace14a17289018a58f7eb8f1332ef6be
liamoconnor87 <liamoconnor87@gmail.com>
2021-04-15 17:03:06 +0000

    Shopping Bag quantity form created. No functionality.

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 def view_bag(request):
@@ -23,3 +23,38 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_quantity(request, item_id):
+    # Adjusts quantity of product to shopping bag.
+
+    quantity = int(request.POST.get('quantity'))
+    # Retrieves bag items if it exist, if not creates it. 
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id]= quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_product_from_bag(request, item_id):
+    # Removes Product from Shopping Bag. 
+
+    try:
+        quantity = int(request.POST.get('quantity'))
+        # Retrieves bag items if it exist, if not creates it. 
+        bag = request.session.get('bag', {})
+
+        if quantity > 0:
+            bag[item_id]= quantity
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(Status=500)
