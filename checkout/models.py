1339ac84e45015ae621dea9b3e5b023a53ba5420
liamoconnor87 <liamoconnor87@gmail.com>
2021-04-26 23:49:35 +0000

    Added forms, admin and signals.

--- checkout/models.py
+++ checkout/models.py
@@ -8,6 +8,7 @@ from products.models import Product
 
 # Create your models here.
 class Order(models.Model):
+    
     order_number = models.CharField(max_length=32, null=False, editable=False)
     # user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
---
+++ checkout/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    # Updates the order title on lineitem update/create
+
+   instance.order.update_total() 
+
+
+@receiver(post_delete, sender=OrderLineItem)
+def delete_line_item(sender, instance, **kwargs):
+    # Updates the order title on lineitem delete
+
+   instance.order.update_total() 
