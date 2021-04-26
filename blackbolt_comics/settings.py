fc02c1a280cef74efac4eefe54c25107a55dbd8d
liamoconnor87 <liamoconnor87@gmail.com>
2021-04-26 19:11:55 +0000

    Created checkapp and models.

--- blackbolt_comics/settings.py
+++ blackbolt_comics/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'home',
     'products',
     'shopping_bag',
+    'checkout',
 ]
 
 MIDDLEWARE = [
--- products/models.py
+++ products/models.py
@@ -27,8 +27,8 @@ class Product(models.Model):
     artist = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024)
-    image = models.ImageField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
