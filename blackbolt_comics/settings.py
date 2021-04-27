34adde5d9fbb9d5fdc3f8eb56056ffbeda55f047
liamoconnor87 <liamoconnor87@gmail.com>
2021-04-27 13:50:43 +0000

    Inital checkout app and views and form.

--- blackbolt_comics/settings.py
+++ blackbolt_comics/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'products',
     'shopping_bag',
     'checkout',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +60,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'blackbolt_comics.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,8 +77,13 @@ TEMPLATES = [
                 'django.template.context_processors.request', # needed for allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'shopping_bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.contrib import messages
+
+from .forms import OrderForm
+from .models import Order, OrderLineItem
 
 # Create your views here.
+def checkout(request):
+
+    bag = request.session.get('bag', {})
+
+    if not bag:
+        messages.error(request, "There's nothing in your bag!")
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
