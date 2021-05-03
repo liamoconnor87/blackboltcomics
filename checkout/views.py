d63b1bb428fcc45f1b4755470dd0577e98b039c0
liamoconnor87 <liamoconnor87@gmail.com>
2021-05-03 11:11:48 +0000

    Added Stripe elements in Checkout / Fix: Form labelling.

--- checkout/views.py
+++ checkout/views.py
@@ -17,6 +17,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51ImyONJVamNXdHot0tWHxFCdtticHDo2ptp1xh7PBcvHFHygdddE0rHfVlfftbCDazwqMOBJCQPDyuQh1JwlI8pd007n9tXWbH',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
