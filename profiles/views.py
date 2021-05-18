536c015b30daa6527fcf7dc9c48167bed033d4b5
liamoconnor87 <liamoconnor87@gmail.com>
2021-05-18 10:41:27 +0000

    Tested profiles.

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
 
 
 def profile(request):
-    # Displays the User's Profile. 
+    # Displays the User's Profile.
+    profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
