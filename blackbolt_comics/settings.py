119a7a3fa523cd234c8f36e27b1ab3abe9c6c446
liamoconnor87 <liamoconnor87@gmail.com>
2021-05-21 14:07:08 +0000

    Amended settings.py for heroku.

--- blackbolt_comics/settings.py
+++ blackbolt_comics/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True # 'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['localhost',  'blackbolt-comics.herokuapp.com']
+ALLOWED_HOSTS = ['blackbolt-comics.herokuapp.com', 'localhost']
 
 
 # Application definition
