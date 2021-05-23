5fecf71092dc9df275244673fbde402f3b53d66a
liamoconnor87 <liamoconnor87@gmail.com>
2021-05-23 10:54:44 +0000

    Fix: now EMAIL_USE_TLS.

--- blackbolt_comics/settings.py
+++ blackbolt_comics/settings.py
@@ -237,10 +237,9 @@ else:
 if 'DEVELOPMENT' in os.environ:
     print('GMAIL SETTINGS APPLIED.')
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USER_TLS = True
+    EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
-    print(EMAIL_HOST, EMAIL_HOST_USER, DEFAULT_FROM_EMAIL)
